e7a3f93999e51773297c0891be5999ddcfaa8cd4
Jennifer <z5118780@unsw.edu.au>
2018-10-25 08:43:42 +1100

    add youtube app

---
+++ app/src/main/java/com/example/jennwong/smartup/YoutubeMainActivity.java
@@ -0,0 +1,88 @@
+package com.example.jennwong.smartup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.ErrorReason;
+import com.google.android.youtube.player.YouTubePlayer.PlayerStateChangeListener;
+import com.google.android.youtube.player.YouTubePlayer.PlaybackEventListener;
+import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.ErrorReason;
+import com.google.android.youtube.player.YouTubePlayer.PlayerStateChangeListener;
+import com.google.android.youtube.player.YouTubePlayer.PlaybackEventListener;
+import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import java.util.List;
+public class YoutubeMainActivity extends YouTubeBaseActivity {
+
+
+        private static final String TAG = "MainActivity";
+
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Log.d(TAG, "onCreate: Startin,");
+            Button BtnP =  findViewById(R.id.button);
+            final YouTubePlayerView mYoutubePlayerView = (YouTubePlayerView) findViewById(R.id.youtubePlayerViewId);
+
+
+            final OnInitializedListener mOnInitializedListener = new OnInitializedListener() {
+
+
+                @Override
+                public void onInitializationSuccess(Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                    Log.d(TAG, "onClick, Done Initialising");
+                    youTubePlayer.loadVideo ("S43CFcpOZSI");
+                }
+
+                @Override
+                public void onInitializationFailure(Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                    Log.d(TAG, "onClick, Failed to Initialise");
+
+                }
+
+
+            };
+
+            BtnP.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v)
+                {
+                    Log.d(TAG,"OnClick: Initialising YouTube Player");
+                    mYoutubePlayerView.initialize(youtubeConfig.getApiKey(),mOnInitializedListener);
+
+
+                }
+            });
+
+
+
+
+
+
+
+
+        }
+
+
+    }
---
+++ app/src/main/java/com/example/jennwong/smartup/youtubeConfig.java
@@ -0,0 +1,15 @@
+package com.example.jennwong.smartup;
+
+public class youtubeConfig {
+
+        public youtubeConfig() {
+        }
+
+        public static final String API_KEY = "AIzaSyDvGemiW83S-UPT_gz_Z-tHxyboOciAGx4";
+
+        public static String getApiKey() {
+            return API_KEY;
+        }
+    }
+
+}
