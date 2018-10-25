56c39bdba8c8c78904bea878babded4306604395
Jennifer <z5118780@unsw.edu.au>
2018-10-25 20:05:15 +1100

    add notes

---
+++ app/src/main/java/com/example/jennwong/smartup/Notes.java
@@ -0,0 +1,33 @@
+package com.example.jennwong.smartup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+
+public class Notes extends AppCompatActivity {
+
+    
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+
+            Button play = (Button) findViewById(R.id.btn);
+            play.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(MainActivity.this,YoutubeMainActivity.class);
+                    startActivity(i);
+                }
+
+            });
+        }
+
+    }
